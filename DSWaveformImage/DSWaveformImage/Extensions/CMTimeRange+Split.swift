448d0d4cd3507c4f9e679984e346d80bf02cf9ec
Dmitry Nuzhin <iruirc@gmail.com>
2022-11-09 14:06:13 +0300

    CMTimeRange. Splitting for a given number of segments of equal length

--- DSWaveformImage/DSWaveformImage/Extensions/CMTimeRange+Split.swift
+++ DSWaveformImage/DSWaveformImage/Extensions/CMTimeRange+Split.swift
@@ -11,6 +11,29 @@ import CoreMedia
 
 extension CMTimeRange {
 
+    /// Splitting CMTimeRange for a given number of segments of equal length
+    func splitIntoChunks(count: UInt) -> [CMTimeRange]? {
+        if count == 0 { return nil }
+        if count == 1 { return [self] }
+        
+        // interval - длительность подинтервалов
+        // duration = duration.value/duration.timescale
+        // interval = duration.value/(duration.timescale * count)
+        let interval = CMTimeMultiplyByRatio(duration, multiplier: 1, divisor: Int32(count))
+        
+        var chunks: [CMTimeRange] = []
+        var from = self.start
+        for _ in 0..<count {
+            let chunk = CMTimeRange(start: from, duration: interval)
+            chunks.append(chunk)
+            from = from + interval  // swiftlint:disable:this shorthand_operator
+        }
+        return chunks.count > 0 ? chunks : nil
+    }
+    
+    /// Splitting CMTimeRange into segments of a given length
+    /// - Note: At the same time, if length is not a multiple of duration for a given CMTimeRange (which usually happens),
+    /// then the last interval will be shorter than the specified length
     func splitIntoChunks(length: CMTime) -> [CMTimeRange]? {
         var chunks: [CMTimeRange] = []
         var from = self.start
