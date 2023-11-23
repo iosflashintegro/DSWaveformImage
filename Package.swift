0fd21ca2148aa786f11e498970fdbfdd964e1431
Dmitry Nuzhin <iruirc@gmail.com>
2023-11-23 14:12:17 +0300

    Package config. Add dependency: vsdcCommonServices

--- Package.swift
+++ Package.swift
@@ -15,15 +15,16 @@ let package = Package(
             targets: ["DSWaveformImage"])
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "git@github.com:iosflashintegro/vsdcCommonServices.git", from: "0.1.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "DSWaveformImage",
-            dependencies: [],
+            dependencies: [
+                .product(name: "vsdcCommonServices", package: "vsdcCommonServices")
+            ],
             path: "DSWaveformImage/DSWaveformImage")
     ]
 )
